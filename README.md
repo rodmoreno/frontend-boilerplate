2621b27bc636cc74f32e3c78b64e1bada5214f1e
Rodrigo Moreno <moreno.rdr@gmail.com>
2017-04-21 02:28:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Frontend Boilerplate
+
+A boilerplate of things that mostly shouldn't exist.
+Based on @tj [Frontend Boilerplate](https://github.com/tj/frontend-boilerplate) using [Neutrino](https://neutrino.js.org)
+
+## Contains
+
+- [x] [Neutrino](https://neutrino.js.org)
+- [x] [React](https://facebook.github.io/react/)
+- [x] [Redux](https://github.com/reactjs/redux)
+- [x] [Autoprefixer](https://github.com/postcss/autoprefixer)
+- [x] [PostCSS](https://github.com/postcss/postcss)
+- [x] [CSS modules](https://github.com/outpunk/postcss-modules)
+- [x] [Rucksack](http://simplaio.github.io/rucksack/docs)
+- [x] [React Router Redux](https://github.com/reactjs/react-router-redux)
+- [x] [Redux DevTools Extension](https://github.com/zalmoxisus/redux-devtools-extension)
+- [ ] Redux effects
+- [x] TodoMVC example
+
+## Setup
+
+```
+$ yarn install
+```
+
+## Running
+
+```
+$ yarn start
+```
+
+## Build
+
+```
+$ yarn build
+```
+
+## Note
+
+"My personal projects have diverged from this quite a bit, I use browserify now instead etc, but feel free to use this if it fits your needs! I won't be updating it a ton for now unless I have time to update it to match my current workflow." - @tj
+
+# License
+
+MIT
---
+++ src/components/TodoTextInput/index.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import classnames from 'classnames'
+import style from './style.css'
+
+class TodoTextInput extends Component {
+  constructor (props, context) {
+    super(props, context)
+    this.state = {
+      text: this.props.text || ''
+    }
+  }
+
+  handleSubmit (e) {
+    const text = e.target.value.trim()
+    if (e.which === 13) {
+      this.props.onSave(text)
+      if (this.props.newTodo) {
+        this.setState({ text: '' })
+      }
+    }
+  }
+
+  handleChange (e) {
+    this.setState({ text: e.target.value })
+  }
+
+  handleBlur (e) {
+    const text = e.target.value.trim()
+    if (!this.props.newTodo) {
+      this.props.onSave(text)
+    }
+  }
+
+  render () {
+    const classes = classnames({
+      [style.edit]: this.props.editing,
+      [style.new]: this.props.newTodo
+    }, style.normal)
+
+    return (
+      <input className={classes}
+        type="text"
+        autoFocus="true"
+        placeholder={this.props.placeholder}
+        value={this.state.text}
+        onBlur={this.handleBlur.bind(this)}
+        onChange={this.handleChange.bind(this)}
+        onKeyDown={this.handleSubmit.bind(this)} />
+    )
+  }
+}
+
+export default TodoTextInput
