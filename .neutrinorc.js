166bbedf88966e2187f5fc200cc25378e6f15a5f
Rodrigo Moreno <rodrigo@iMac-de-Angel.local>
2017-08-31 19:26:59 -0500

    ! Update to Neutrino 6.1.5

---
+++ .neutrinorc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  use: [
+    ['neutrino-preset-react',
+      neutrino => neutrino.config
+        .entry('vendor')
+          .add('react')
+          .add('react-dom')
+          .add('react-redux')
+          .add('react-router')
+          .add('react-router-redux')
+          .add('redux')
+    ],
+    neutrino => neutrino.config.module.rule('style').use('css').options({ modules: true }),
+    ['neutrino-preset-react', {
+      plugins: {
+        'rucksack-css': {autoprefixer: true}
+      }
+    }]
+  ]
+}
+  
