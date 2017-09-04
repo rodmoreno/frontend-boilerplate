a0a84e31e6b13628ed9a3550b665ca91ef701f95
Rodrigo Moreno <moreno.rdr@gmail.com>
2017-09-04 16:09:47 -0500

    ^ typo

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,15 +1,14 @@
 module.exports = {
   use: [
-    ['neutrino-preset-react',
-      neutrino => neutrino.config
-        .entry('vendor')
-          .add('react')
-          .add('react-dom')
-          .add('react-redux')
-          .add('react-router')
-          .add('react-router-redux')
-          .add('redux')
-    ],
+    'neutrino-preset-react',
+    neutrino => neutrino.config
+      .entry('vendor')
+        .add('react')
+        .add('react-dom')
+        .add('react-redux')
+        .add('react-router')
+        .add('react-router-redux')
+        .add('redux'),
     neutrino => neutrino.config.module.rule('style').use('css').options({ modules: true }),
     ['neutrino-preset-react', {
       plugins: {
